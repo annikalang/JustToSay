4cf0aa3d7896a7659fc150cd0f86bcc3d418180a
Annika Lang <Lang-annika@t-online.de>
2020-04-19 17:27:01 +0200

    added routing and profile method in employee controller

--- app/controllers/employee_controller.rb
+++ app/controllers/employee_controller.rb
@@ -1,2 +1,8 @@
 class EmployeeController < ApplicationController
+
+  def profile
+    # here, the first created user is selected, this has to be changed since an authentication service is implemented -> Devise)
+    @user = User.(where: id = 1)
+
+  end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,9 +1,4 @@
 class Employee < ApplicationRecord
   has_many :appreciations
   has_many :appreciations, through: :sppreciation_receptions
-
-  def profile
-
-  end
-
 end
